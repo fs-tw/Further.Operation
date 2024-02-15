faa42a5902c624f1c8055ccfe4343fe1b94c28ce
ggoch <loyuqingan@gmail.com>
2024-02-15 18:40:38 +0800

    feat: add test

---
+++ src/Further.Abp.Operation/Further/Abp/Operation/OperationScopeOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Further.Abp.Operation
+{
+    public class OperationScopeOptions
+    {
+        public bool SaveToCache { get; set; } = false;
+
+        public bool EnableLogger { get; set; } = false;
+    }
+}
---
+++ test/Further.Abp.Operation.Test/Further/Abp/Operation/OperationScopeProviderTest.cs
@@ -0,0 +1,121 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Testing;
+using Volo.Abp.Threading;
+using Xunit;
+
+namespace Further.Abp.Operation
+{
+    public class OperationScopeNestedTest : AbpIntegratedTest<FurtherAbpOperationTestBaseModule>
+    {
+        private readonly IOperationScopeProvider operationScopeProvider;
+
+        public OperationScopeNestedTest()
+        {
+            operationScopeProvider = GetRequiredService<IOperationScopeProvider>();
+        }
+
+        protected override void SetAbpApplicationCreationOptions(AbpApplicationCreationOptions options)
+        {
+            options.UseAutofac();
+        }
+
+        [Fact]
+        public async Task Should_Create_Nested_OperationScope()
+        {
+            operationScopeProvider.Current.ShouldBeNull();
+
+            using (var scope1 = operationScopeProvider.Begin(value: new OperationInfoInitializeValue { OperationName = "Scope1" }))
+            {
+                operationScopeProvider.Current.ShouldNotBeNull();
+                operationScopeProvider.Current.ShouldBe(scope1.OperationInfo);
+                operationScopeProvider.Current.OperationName.ShouldBe("Scope1");
+
+                using (var scope2 = operationScopeProvider.Begin(value: new OperationInfoInitializeValue { OperationName = "Scope2" }))
+                {
+                    operationScopeProvider.Current.ShouldNotBeNull();
+                    operationScopeProvider.Current.ShouldBe(scope2.OperationInfo);
+                    operationScopeProvider.Current.OperationName.ShouldBe("Scope2");
+
+                    await scope2.CompleteAsync();
+                }
+
+                operationScopeProvider.Current.ShouldNotBeNull();
+                operationScopeProvider.Current.ShouldBe(scope1.OperationInfo);
+                operationScopeProvider.Current.OperationName.ShouldBe("Scope2");
+
+                await scope1.CompleteAsync();
+            }
+
+            operationScopeProvider.Current.ShouldBeNull();
+        }
+
+        [Fact]
+        public async Task Should_Create_Nested_ReqiiredNew_OperationScope()
+        {
+            operationScopeProvider.Current.ShouldBeNull();
+
+            using (var scope1 = operationScopeProvider.Begin(value: new OperationInfoInitializeValue { OperationName = "Scope1" }))
+            {
+                operationScopeProvider.Current.ShouldNotBeNull();
+                operationScopeProvider.Current.ShouldBe(scope1.OperationInfo);
+                operationScopeProvider.Current.OperationName.ShouldBe("Scope1");
+
+                using (var scope2 = operationScopeProvider.Begin(value: new OperationInfoInitializeValue { OperationName = "Scope2" }, requiresNew: true))
+                {
+                    operationScopeProvider.Current.ShouldNotBeNull();
+                    operationScopeProvider.Current.ShouldBe(scope2.OperationInfo);
+                    operationScopeProvider.Current.OperationName.ShouldBe("Scope2");
+
+                    await scope2.CompleteAsync();
+                }
+
+                operationScopeProvider.Current.ShouldNotBeNull();
+                operationScopeProvider.Current.ShouldBe(scope1.OperationInfo);
+                operationScopeProvider.Current.OperationName.ShouldBe("Scope1");
+
+                await scope1.CompleteAsync();
+            }
+
+            operationScopeProvider.Current.ShouldBeNull();
+        }
+
+        [Fact]
+        public async Task EnsureConcurrentAccessToScopeProviderBehavesCorrectly()
+        {
+            var tasks = new List<Task>();
+
+            int numberOfTasks = 20;
+            for (int i = 0; i < numberOfTasks; i++)
+            {
+                var value = i.ToString();
+                tasks.Add(Task.Run(async () =>
+                {
+                    using (var scope = operationScopeProvider.Begin(value: new OperationInfoInitializeValue { OperationName = $"Scope{value}" }, requiresNew: true))
+                    {
+                        operationScopeProvider.Current.ShouldNotBeNull();
+                        operationScopeProvider.Current.ShouldBe(scope.OperationInfo);
+                        operationScopeProvider.Current.OperationName.ShouldBe($"Scope{value}");
+
+                        var changed = $"changed{value}";
+
+                        operationScopeProvider.Current.OperationName = changed;
+
+                        Task.Delay(10).Wait();
+
+                        operationScopeProvider.Current.OperationName.ShouldBe(changed);
+
+                        await scope.CompleteAsync();
+                    }
+                }));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
