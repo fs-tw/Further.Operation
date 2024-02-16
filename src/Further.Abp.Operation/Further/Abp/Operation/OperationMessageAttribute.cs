078ccaabe930a489ee1fa421d9f03adf50e1bdaa
ggoch <loyuqingan@gmail.com>
2024-02-16 15:30:14 +0800

    feat: 添加 OperationAttribute

--- src/Further.Abp.Operation/Further/Abp/Operation/OperationMessageAttribute.cs
+++ src/Further.Abp.Operation/Further/Abp/Operation/OperationMessageAttribute.cs
@@ -5,24 +5,29 @@ using System.Text;
 
 namespace Further.Abp.Operation
 {
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Interface)]
-    public class OperationMessageAttribute: OperationAttributeBase
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class OperationMessageAttribute: OperationInfoAttributeBase
     {
-        public string? Message { get; set; }
+        public string Message { get; set; }
 
         public override void UpdateOperationInfo(OperationInfo operationInfo, object methodResult)
         {
-            if (Message != null)
-            {
-                var success = new Success(Message);
+            var success = new Success(Message);
 
-                foreach (var metadata in Metadata)
-                {
-                    success.WithMetadata(metadata.Key, metadata.Value);
-                }
+            SetMetadata(success);
 
-                operationInfo?.Result.WithSuccess(success);
-            }
+            operationInfo?.Result.WithSuccess(success);
+        }
+
+        public OperationMessageAttribute(string message)
+        {
+            this.Message = message;
+        }
+
+        public OperationMessageAttribute(string message, Dictionary<string, object> metadata)
+        {
+            this.Message = message;
+            this.Metadata = metadata;
         }
     }
 }
