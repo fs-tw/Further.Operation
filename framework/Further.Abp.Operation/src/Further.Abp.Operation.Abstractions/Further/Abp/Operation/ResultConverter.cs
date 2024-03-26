af8122d1abb30c09b3b0115af18fcdc5ce2b7914
ggoch <loyuqingan@gmail.com>
2024-03-26 15:18:20 +0800

    feat: abp 降板成5.3.1

--- framework/Further.Abp.Operation/src/Further.Abp.Operation.Abstractions/Further/Abp/Operation/ResultConverter.cs
+++ framework/Further.Abp.Operation/src/Further.Abp.Operation.Abstractions/Further/Abp/Operation/ResultConverter.cs
@@ -8,14 +8,17 @@ namespace Further.Abp.Operation
 {
     public class ResultConverter : JsonConverter<IResultBase>
     {
-        private readonly static JsonConverter<ResultDto> defaultConverter =
-            (JsonConverter<ResultDto>)JsonSerializerOptions.Default.GetConverter(typeof(ResultDto));
+        //此為net8專用寫法
+        //private readonly static JsonConverter<ResultDto> defaultConverter =
+        //    (JsonConverter<ResultDto>)JsonSerializerOptions.Default.GetConverter(typeof(ResultDto));
         public override IResultBase Read(
             ref Utf8JsonReader reader,
             Type typeToConvert,
             JsonSerializerOptions options)
         {
-            var obj = defaultConverter.Read(ref reader, typeof(ResultDto), options)!;
+            //var obj = defaultConverter.Read(ref reader, typeof(ResultDto), options)!;
+
+            var obj = JsonSerializer.Deserialize<ResultDto>(ref reader, options);
 
             var result = Result.Ok();
 
@@ -40,7 +43,30 @@ namespace Further.Abp.Operation
             IResultBase value,
             JsonSerializerOptions options)
         {
-            JsonSerializer.Serialize(writer, (object)value, options);
+            //JsonSerializer.Serialize(writer, (object)value, options);
+
+            var target = new ResultDto
+            {
+                IsFailed = value.IsFailed,
+                IsSuccess = value.IsSuccess,
+                Errors = value.Errors.Select(e => new ResultDto.Error
+                {
+                    Message = e.Message,
+                    Metadata = e.Metadata.ToDictionary(m => m.Key, m => m.Value),
+                    Reasons = e.Reasons.Select(r => new ResultDto.Error
+                    {
+                        Message = r.Message,
+                        Metadata = r.Metadata.ToDictionary(m => m.Key, m => m.Value)
+                    }).ToList()
+                }).ToList(),
+                Successes = value.Successes.Select(s => new ResultDto.Success
+                {
+                    Message = s.Message,
+                    Metadata = s.Metadata.ToDictionary(m => m.Key, m => m.Value)
+                }).ToList()
+            };
+
+            JsonSerializer.Serialize(writer, target, options);
         }
     }
 }
